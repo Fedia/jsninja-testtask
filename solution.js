92280ee63941fba9a3fed840e10cb03eb6343f1f
Fedor <fedia@psih.ru>
2019-04-10 02:18:35 +0300

    Always return a boolean value

--- solution.js
+++ solution.js
@@ -2,5 +2,5 @@ function findWord(text, data) {
   const rx = RegExp(text + '|' + Array.from(text).reverse().join(''));
   if (data.join().match(rx)) return true;
 	const dataT = Array.from(data[0], (_, i) => data.map(row => row[i]).join(''));
-	return dataT.join().match(rx);
+	return !!dataT.join().match(rx);
 }
